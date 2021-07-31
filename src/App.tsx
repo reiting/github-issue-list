abecc38cb0d773a647a432ecce5afdd67b2ac364
Rachel Eiting <reiting1@gmail.com>
2021-07-30 20:07:28 -0500

    Changed this to issue list instead of item. Still have testing strings in here

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import ListItem from './components/ListItem';
+import IssueList from './components/IssueList';
 
 const App = () => {
   return (
-    <ListItem />
-  )
+    <IssueList owner='microsoft' repo='TypeScript' />  )
 }
 
 export default App;
