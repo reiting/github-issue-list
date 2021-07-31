ccc02cd24e89f789928eb61920135e5ef706cefc
Rachel Eiting <reiting1@gmail.com>
2021-07-30 20:08:23 -0500

    Added things I want to render in ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,11 +1,15 @@
-import IssueList from "./IssueList"
 
-const ListItem: React.FC = () => {
+const ListItem = ({title, html_url, user, repository, repository_url, node_id}: ApiData) => {
 
   return (
-    <IssueList owner='microsoft' repo='TypeScript' />
+    <>
+   <li><a href={html_url}>{title}</a></li>
+   <p>{user.login}</p>
+   <p>{user.avatar_url}</p>
+   <p>{repository}</p>
+   <p>{repository_url}</p>
+   </>
   )
 }
-
 
 export default ListItem
