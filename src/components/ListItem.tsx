ae3db09f8a143d84fbd77f39445f5b7a7db13471
Rachel Eiting <reiting1@gmail.com>
2021-07-31 07:56:09 -0500

    Made the items links in the ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -4,10 +4,9 @@ const ListItem = ({title, html_url, user, repository, repository_url, node_id}: 
   return (
     <>
    <li><a href={html_url}>{title}</a></li>
-   <p>{user.login}</p>
-   <p>{user.avatar_url}</p>
-   <p>{repository}</p>
-   <p>{repository_url}</p>
+   <p><a href={repository}>{user.login}</a></p>
+   <p><a href={repository}>{user.avatar_url}</a></p>
+   <p><a href={repository_url}>REpo Name</a></p>
    </>
   )
 }
