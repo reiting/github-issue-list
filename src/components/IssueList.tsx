92efa585d97790bbcc313da5bf6d813db1ee6b0e
Rachel Eiting <reiting1@gmail.com>
2021-08-01 14:38:28 -0500

    Sorted issues alphabetically Added delete button and function changed some things being passed to ListItem

--- src/components/IssueList.tsx
+++ src/components/IssueList.tsx
@@ -2,60 +2,55 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import ListItem from "./ListItem"
 
-declare module 'axios' {
-  export interface AxiosRequestConfig {
-    owner: string;
-    repo: string;
-  }
-}
-
-const IssueList = ({ owner, repo }: Data) => {
+const IssueList = () => {
   const [issues, setIssues] = useState<ApiData[]>([])
 
-  const fetchApiData = async () => {
+  const fetchApiData = async () => 
+  {
     try {
       const [typeScriptApiData, reactApiData, graphqlApiData] = await Promise.all([
-        axios.get(`https://api.github.com/repos/${owner}/${repo}/issues`, { owner: 'microsoft', repo: 'TypeScript' }),
-        axios.get(`https://api.github.com/repos/${owner}/${repo}/issues`, { owner: 'facebook', repo: 'react' }),
-        axios.get(`https://api.github.com/repos/${owner}/${repo}/issues`, { owner: 'graphql', repo: 'graphql-js' }),
+        axios.get(`https://api.github.com/repos/microsoft/TypeScript/issues`),
+        axios.get(`https://api.github.com/repos/facebook/react/issues`),
+        axios.get(`https://api.github.com/repos/graphql/graphql-js/issues`),
       ])
+
       let combinedIssues = [...typeScriptApiData.data, ...reactApiData.data, ...graphqlApiData.data]
-      console.log('comined', combinedIssues)
-      console.log('issues', issues)
-      setIssues(combinedIssues)
+      let filteredIssues = combinedIssues.filter(issue => !Boolean(issue.pull_request))
+      filteredIssues.sort((a, b) => (a.title > b.title) ? 1 : -1)
+      setIssues(filteredIssues)
     }
     catch (err) {
       console.log(err)
     }
   }
 
+  let deletedIssue = (id: number) => {
+    let issuesToKeep = issues.filter(issue => issue.id !== id)
+    setIssues(issuesToKeep)
+  }
+
+
   useEffect(() => {
       fetchApiData()
   }, [])
-
-//   const alphabetize = (a: string, b: string) => {
-//     if(a.title < b.title) { return -1; }
-//     if(a.title > b.title) { return 1; }
-//     return 0;
-// }
 
   return (
     <div>
       <h1>Issue List from Github</h1>
         {issues.map(issue => (
-      <ul key={issue.node_id}
+      <ul key={issue.id}
       >
           <ListItem
             title={issue.title}
             html_url={issue.html_url}
             user={issue.user}
             userAvatar={issue.user.avatar_url}
-            repository={issue.user.url}
-            repository_url={issue.repository_url}
-            node_id={issue.node_id}
+            url={issue.url}
+            id={issue.id}
           />
+      <button onClick={() => {deletedIssue(issue.id)}}>Delete</button>
       </ul>
-        ))}
+       ))}
     </div>
   )
 }
