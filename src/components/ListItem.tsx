c04b913c29b9243e6c1db24f7e02ff1a12d57605
Rachel Eiting <reiting1@gmail.com>
2021-08-01 10:08:03 -0500

    Added titles to all the components for clarity

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,12 +1,14 @@
 
 const ListItem = ({title, html_url, user, repository, repository_url, node_id}: ApiData) => {
 
+  let repo = repository_url.split('/')
+  console.log('REP', repo)
   return (
     <>
-   <li><a href={html_url}>{title}</a></li>
-   <p><a href={repository}>{user.login}</a></p>
-   <p><a href={repository}>{user.avatar_url}</a></p>
-   <p><a href={repository_url}>REpo Name</a></p>
+   <li>Issue: <a href={html_url}>{title}</a></li>
+   <p>Username: <a href={repository}>{user.login}</a></p>
+   <p>User avatar: <a href={repository}>{user.avatar_url}</a></p>
+   <p>Issue Repository: <a href={repository_url}>{repo[5]}</a></p>
    </>
   )
 }
